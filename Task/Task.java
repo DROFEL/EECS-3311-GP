7c11910ff059e24db6e38441fe4041a57064b176
Khoshpasand-mehrsa <mkhoshpa@my.yorku.ca>
2023-02-01 15:50:59 -0500

    task class

--- Task/Task.java
+++ Task/Task.java
@@ -1,3 +1,15 @@
-Task{
-    
+public class Task{
+    String taskID;
+    String name;
+    String status;
+    String comments;
+
+    pubic void editStatus(){
+
+    }
+
+    public void addComment(){
+        
+    }
+
 }
