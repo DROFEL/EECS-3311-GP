8a46cd1fa8e10e54f4a653b98b71efc258908de4
Khoshpasand-mehrsa <mkhoshpa@my.yorku.ca>
2023-02-01 17:51:04 -0500

    added to Task

--- Task/Task.java
+++ Task/Task.java
@@ -1,15 +1,67 @@
+import Team.java; //todo; implement Team
 public class Task{
     String taskID;
     String name;
     String status;
     String comments;
+    Team team; 
 
-    pubic void editStatus(){
-
+    //constructor
+    public Task (){
+        this.taskID = "Null";
+        this.name = "Null";
+        this.status = "Null";
+        this.comments = '';
+        this.team = null; 
+    }
+    public Task (String taskID, String name, String status, String comments, Team team){
+        this.taskID = taskID;
+        this.name = name;
+        this.status = status;
+        this.comments = comments;
+        this.team = team;
     }
 
-    public void addComment(){
-        
+    //getters
+    public String getID(){
+        return this.taskID;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public String getStatus(){
+        return this.status;
+    }
+    public String getComments(){
+        return this.comments;
+    }
+    public Team getTeam(){
+        return this.team;
+    }
+
+
+    //setters
+    public void setID(String taskID){
+        this.taskID = taskID;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void assignTeam(Team team){
+        this.team = team;
+    }
+
+
+    //additional
+    public void editStatus(String status){
+        this.status = status;
+    }
+
+    public void addComment(String newComment){
+        if (comments != ''){
+            comments += ", ";
+        }
+        comments += newComment;
     }
 
 }
