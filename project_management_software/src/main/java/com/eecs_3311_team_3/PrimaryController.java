5ae09ba0c71d5b462d71944058487f64310849b0
George <cradplay@gmail.com>
2023-02-08 23:00:52 -0500

    repo fix: regenerated project, installed maven, added .gitignore, installed javafx

---
+++ project_management_software/src/main/java/com/eecs_3311_team_3/PrimaryController.java
@@ -0,0 +1,12 @@
+package com.eecs_3311_team_3;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
---
+++ project_management_software/src/main/java/com/eecs_3311_team_3/SecondaryController.java
@@ -0,0 +1,12 @@
+package com.eecs_3311_team_3;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
