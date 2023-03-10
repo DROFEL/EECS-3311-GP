4e026f661e8ce7285586c3b9c5efc96ffa507e19
jazzinyourface <108697176+jazzinyourface@users.noreply.github.com>
2023-03-10 02:06:37 -0500

    Add files via upload

---
+++ project_management_software/src/main/java/com/eecs_3311_team_3/DatabaseConnection.java
@@ -0,0 +1,23 @@
+package com.eecs_3311_team_3;
+
+import java.sql.*;
+
+public class DatabaseConnection {
+	public Connection databaseLink;
+	
+	public Connection getConnection() {
+		String databaseName = "pms";
+		String databaseUser = "root";
+		String databasePassword = "1234";
+		String url = "jdbc:mysql://localhost/" + databaseName;
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			databaseLink = DriverManager.getConnection(url, databaseUser, databasePassword);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return databaseLink;
+	}
+}
